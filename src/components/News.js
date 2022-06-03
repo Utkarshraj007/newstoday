24c9d2f2c502f6869b6cd13478cd22ffd74e224e
Utkarshraj007 <59512749+Utkarshraj007@users.noreply.github.com>
2022-06-04 01:08:52 +0530

    first commit

---
+++ src/components/News.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+
+export class News extends Component {
+  capitalizeFirstLetter = string => string.charAt(0).toUpperCase() + string.slice(1);
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            articles:[],
+            page:1,
+            // totalArticles:
+        }
+        document.title=`${this.capitalizeFirstLetter(this.props.category)}-NewsToday`;
+    }
+    async componentDidMount(){
+        let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=743caac43e72408f8f793f9046c49967&page=1&pageSize=${this.props.pageSize}`;
+        let data=await fetch(url);
+        let parsedData= await data.json();
+        this.setState({articles:parsedData.articles , totalArticles:parsedData.totalResults})
+
+    }
+    handlePrevClick = async()=>{
+
+        let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=743caac43e72408f8f793f9046c49967&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        let data=await fetch(url);
+        let parsedData= await data.json(); 
+        this.setState({
+            page:this.state.page - 1,
+            articles:parsedData.articles
+        })
+    } 
+    handleNextClick = async()=>{
+        if(this.state.page + 1 > Math.ceil(this.state.totalArticles/this.props.pageSize )){}
+        else{
+            let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=743caac43e72408f8f793f9046c49967&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+            let data=await fetch(url);
+            let parsedData= await data.json(); 
+            this.setState({
+                page:this.state.page + 1,
+                articles: parsedData.articles
+            })
+        }
+       
+    }
+  render() {
+    return (
+      <div className="container my-3 ">
+        <h2 className="text-center">NewsToday - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h2>
+        <div className="row">
+        {this.state.articles.map(element=>{
+            return <div className="col-md-4" key={element.url}>
+            <NewsItem  title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author?element.author:`By Unknown`} date={element.publishedAt} source={element.source.name}/>
+          </div>
+        })}
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" className="btn btn-dark " onClick={this.handlePrevClick}>&larr; Previous</button>
+        <button disabled={this.state.page+1 > Math.ceil(this.state.totalArticles/this.props.pageSize)}type="button" className="btn btn-dark " onClick={this.handleNextClick}>Next &rarr;</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default News;
---
+++ src/components/NewsItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+export class NewsItem extends Component {
+  render() {
+      let {title,description,imageUrl,newsUrl,author,date,source}=this.props;
+    return (
+      <div className="my-3">
+          <div className="card" >
+            <img src={imageUrl?imageUrl:"https://c.ndtvimg.com/2020-12/c8v0o0l8_vistara-airline_625x300_18_December_20.jpg"} className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">{title}<span class="position-absolute top-0 translate-middle badge rounded-pill bg-success" style={{left:'90%',zIndex:'1'}}>{source}</span></h5>
+                <p className="card-text">{description}</p>
+                <p className="card-text"><small className="text-muted"></small>By {author} on {new Date(date).toGMTString()} </p>
+                <a rel="noforever" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+            </div>
+            </div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
